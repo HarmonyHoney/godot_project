85d7989d964e5541ce0f3d3db5700d33ce44c33c
Harmony Honey Monroe <harmonymonroe97@gmail.com>
2024-05-14 22:16:34 -0400

    carvy lerpy powerslidy !

--- player.gd
+++ player.gd
@@ -5,7 +5,6 @@ extends CharacterBody3D
 @onready var axel1 := $Skateboard/Axel
 @onready var axel2 := $Skateboard/Axel2
 
-@export var walk_speed = 5.0
 @export var jump_vel = 4.5
 @export var cam : Node
 
@@ -19,19 +18,17 @@ var btn_brake := false
 var angle := 0.0
 @export var turn_speed := 1.0
 @export var push_speed := 5.0
-@export var push_mod := 0.7
-@export var move_delta := 0.5
 @export var lean_angle := 0.25
 @export var turn_angle := 0.1
 @export var carve_boost := 0.1
-
-var floor_angle = 0.0
 
 var last_turn := 0.0
 var turn_dir := 0.0
 var turn_diff := 0.0
 @export var carve_lerp = 15.0
 @export var turn_lerp := 12.0
+@export var power_lerp := 2.0
+@export var curvy : Curve
 
 func _physics_process(delta):
 	
@@ -42,7 +39,6 @@ func _physics_process(delta):
 	btn_brake = Input.is_action_pressed("brake")
 	
 	if is_floor:
-		floor_angle = angle
 		# pushing
 		if btnp_push:
 			velocity += (Vector3.BACK * push_speed).rotated(Vector3(0, 1, 0), angle)
@@ -72,24 +68,25 @@ func _physics_process(delta):
 	var vang = wrapf(atan2(velocity.x, velocity.z), 0.0, TAU)
 	var q = PI * 0.5
 	var dang = wrapf(vang - angle, -q, q)
-	print(rad_to_deg(angle), " - ", rad_to_deg(vang), " = ", rad_to_deg(dang))
+	var ad = rad_to_deg(abs(dang))
+	var frac = ad / 90.0
+	var unfrac = 1.0 - frac
 	
 	if is_floor:
 		# turn
-		velocity = velocity.rotated(Vector3(0,1,0), -dang * turn_lerp * delta)
+		velocity = velocity.rotated(Vector3(0,1,0), -dang * turn_lerp * curvy.sample(unfrac) * delta)
+		
 		# powerslide
-		if abs(dang) > deg_to_rad(45) or btn_brake:
-			velocity = velocity * 0.95
+		if ad > 33:
+			velocity = velocity * (1.0 - (frac * power_lerp * delta))
+		elif btn_brake:
+			velocity = velocity * (1.0 - (1.0 * power_lerp * delta))
+	
+	print(rad_to_deg(angle), " - ", rad_to_deg(vang), " = ", rad_to_deg(dang), " , ", frac)
 	
 	# velocity
 	$RayCast3D.target_position = velocity.normalized() * 5.0
 	# angle
 	$RayCast3D2.target_position = Vector3(0, 0, 5.0).rotated(Vector3(0,1,0), angle)
-	
-	
-	
-	#vel += turn_diff * (carve_boost * vel)
-	#vel = move_toward(vel, 0.0, move_delta)
-	
 	
 	move_and_slide()
