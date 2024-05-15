125b9ccb6b1d8782e95c8dc4950ef787aa3ecc28
Harmony Honey Monroe <harmonymonroe97@gmail.com>
2024-05-14 21:37:40 -0400

    fuck yeah velocity !

--- player.gd
+++ player.gd
@@ -17,7 +17,6 @@ var btn_push := false
 var btnp_push := false
 var btn_brake := false
 var angle := 0.0
-var vel := 0.0
 @export var turn_speed := 1.0
 @export var push_speed := 5.0
 @export var push_mod := 0.7
@@ -31,7 +30,8 @@ var floor_angle = 0.0
 var last_turn := 0.0
 var turn_dir := 0.0
 var turn_diff := 0.0
-@export var carve_lerp = 1.0
+@export var carve_lerp = 15.0
+@export var turn_lerp := 12.0
 
 func _physics_process(delta):
 	
@@ -44,8 +44,8 @@ func _physics_process(delta):
 	if is_floor:
 		floor_angle = angle
 		# pushing
-		if btnp_push and vel < push_speed:
-			vel = lerp(vel, push_speed, push_mod)
+		if btnp_push:
+			velocity += (Vector3.BACK * push_speed).rotated(Vector3(0, 1, 0), angle)
 	else:	
 		# gravity
 		velocity.y -= gravity * delta
@@ -61,29 +61,35 @@ func _physics_process(delta):
 	turn_dir = lerp(turn_dir, input_dir.x, carve_lerp * delta)
 	turn_diff = abs(turn_dir - last_turn)
 	
-	angle -= turn_dir * turn_speed  * vel
-	skateboard.rotation.y = angle + PI
+	angle -= turn_dir * turn_speed * velocity.length() * delta
+	angle = wrapf(angle, 0.0, TAU)
+	skateboard.rotation.y = angle
 	axel1.rotation.y = -turn_dir * turn_angle
 	axel2.rotation.y = turn_dir * turn_angle
 	# lean
 	deck.rotation.z = turn_dir * lean_angle
 	
+	var vang = wrapf(atan2(velocity.x, velocity.z), 0.0, TAU)
+	var q = PI * 0.5
+	var dang = wrapf(vang - angle, -q, q)
+	print(rad_to_deg(angle), " - ", rad_to_deg(vang), " = ", rad_to_deg(dang))
 	
-	vel += turn_diff * (carve_boost * vel)
-	vel = move_toward(vel, 0.0, move_delta)
+	if is_floor:
+		# turn
+		velocity = velocity.rotated(Vector3(0,1,0), -dang * turn_lerp * delta)
+		# powerslide
+		if abs(dang) > deg_to_rad(45) or btn_brake:
+			velocity = velocity * 0.95
 	
-	if btn_brake:
-		vel = lerp(vel, 0.0, 1.5 * delta)
-	
-	var go = Vector3.FORWARD.rotated(Vector3(0,1,0), floor_angle) * vel
-	
-	velocity.x = go.x
-	velocity.z = go.z
+	# velocity
+	$RayCast3D.target_position = velocity.normalized() * 5.0
+	# angle
+	$RayCast3D2.target_position = Vector3(0, 0, 5.0).rotated(Vector3(0,1,0), angle)
 	
 	
+	
+	#vel += turn_diff * (carve_boost * vel)
+	#vel = move_toward(vel, 0.0, move_delta)
 	
 	
 	move_and_slide()
-	
-	var after = Vector3(velocity.x, 0, velocity.z).length()
-	vel = after
